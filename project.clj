b157ca2dd08764eb42ab933a7defaa53c93b1a7b
Geoff Shannon <geoffpshannon@gmail.com>
2018-09-07 02:23:49 -0700

    Add bar-, baz-, and qux-number functions

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject radz.test/foo "1.0"
+(defproject radz.test/foo "1.2"
   :description ""
   :url "https://github.com/radicalzephyr/radz.test-foo"
   :scm {:url "https://github.com/radicalzephyr/radz.test-foo"}
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [radz.test/bar "1.0"]
-                 [radz.test/baz "1.0"]])
+                 [radz.test/bar "1.2"]
+                 [radz.test/baz "1.1"]])
--- src/radz/test/foo.clj
+++ src/radz/test/foo.clj
@@ -1,3 +1,18 @@
-(ns radz.test.foo)
+(ns radz.test.foo
+  (:require radz.test.bar
+            radz.test.baz
+            radz.test.qux))
 
 (def number 1)
+
+(defn bar-number []
+  radz.test.bar/number)
+
+(defn bar-qux-number []
+  (radz.test.bar/qux-number))
+
+(defn baz-number []
+  radz.test.baz/number)
+
+(defn baz-qux-number []
+  (radz.test.baz/qux-number))
